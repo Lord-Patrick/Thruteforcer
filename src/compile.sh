016140bea81a527ca1e79ed5eae9e60fc1a60abe
Patrick Denz <patrick_denz@hotmail.com>
2024-05-31 21:42:40 +0200

    Nevermind, that works now.

--- src/compile.sh
+++ src/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-g++ -c -o IncreasableString.o IncreasableString.cpp -o2
-g++ -c -o main.o main.cpp -o2
+g++ -c -o IncreasableString.o IncreasableString.cpp -O2
+g++ -c -o main.o main.cpp -O2
 g++ -o main main.o IncreasableString.o -lssl -lcrypto
 ./main
